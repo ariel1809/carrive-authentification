da5163157ec9cf418fe8b0453335a475c75f172a
ariel <Naruto18@>
2024-11-01 21:14:20 +0100

    auth

--- src/main/java/com/manage/carriveauth/CarriveAuthApplication.java
+++ src/main/java/com/manage/carriveauth/CarriveAuthApplication.java
@@ -3,10 +3,8 @@ package com.manage.carriveauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = {"com.manage.carriveutility.repository"})
 @EntityScan("com.manage.carrive")
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class CarriveAuthApplication {
 
     public static void main(String[] args) {
